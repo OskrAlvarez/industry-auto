4066d25c385f36fdf04dc3939db37efb14aeb732
Oscardev <oscardev@gmail.com>
2024-11-20 23:48:29 -0400

    fix the googlemap width on mobile

--- components/sections/GoogleMaps.tsx
+++ components/sections/GoogleMaps.tsx
@@ -17,7 +17,7 @@ export function GoogleMaps() {
           />
         </div>
         <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3311.214734589509!2d-84.22060682398121!3d33.90987252523399!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5a7bae1bfba1b%3A0xca6603a28fc269ad!2sIndustry%20Auto%20LLC!5e0!3m2!1ses!2sve!4v1732029850839!5m2!1ses!2sve" width="450" height="300"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3311.214734589509!2d-84.22060682398121!3d33.90987252523399!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5a7bae1bfba1b%3A0xca6603a28fc269ad!2sIndustry%20Auto%20LLC!5e0!3m2!1ses!2sve!4v1732029850839!5m2!1ses!2sve" width="450" height="300" className="w-[300px] md:w-[450px]"></iframe>
         </div>
       </section>
     </div>
