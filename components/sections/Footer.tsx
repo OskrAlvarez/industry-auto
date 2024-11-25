8866b3ffdef5c59acba4df4b5ba23065b8886919
Oscardev <oscardev@gmail.com>
2024-11-25 10:31:51 -0400

    Add: parking lot direction

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -9,7 +9,8 @@ export function Footer () {
       <div className="absolute w-full inset-0 h-full bg-blue-700/70"></div>
       <div className="relative z-40">
         <p className="text-white text-sm font-normal italic text-center">
-        Industry Auto LLC 6428 Dawson Blvd Ste 1224, Norcross, GA 30093
+        Industry Auto LLC 6428 Dawson Blvd Ste 1224, Norcross, GA 30093 <br />
+        6108 Memorial Dr, Stone Mountain, GA 30083
         </p>
         <div className="social-links flex flex-col items-center md:flex-row  gap-3 mt-4">
           <div className="flex items-center gap-2 text-white">
